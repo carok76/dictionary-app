f378fe2169d187aa7e8c367b3b59b8f9d80ffe4f
carok76 <carolin.krahmer@gmail.com>
2026-01-14 12:40:33 +0100

    Updated links in footer

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Search onSearch={handleSearch} />
 
       <footer>
-        This project was coded by Carolin Krahmer and is on GitHub and hosted on Netlify.
+        This project was coded by <a href="https://www.linkedin.com/in/carolin-krahmer-94661399/" target="_blank" rel="noopener noreferrer">Carolin Krahmer</a> and is on <a href="https://github.com/carok76/dictionary-app" target="_blank" rel="noopener noreferrer">GitHub</a> and hosted on <a href="https://caros-dictionary-app.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>.
       </footer>
     </div>
   );
